1dd25beee7b4faffedf12dbab3e882e0f6ab86c4
Simon Hessel <simon.hessel@kreios.lu>
2020-11-26 19:03:00 +0100

    frontend: added authservice mock and cleanup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public login(usernameOrEmail: string, password: string): Observable<any> {
+    const user = {
+      username: usernameOrEmail.includes('@') ? '' : usernameOrEmail,
+      email: usernameOrEmail.includes('@') ? '' : usernameOrEmail,
+    };
+    return of(user).pipe(
+      mergeMap((user) =>
+        password == '123'
+          ? throwError('Password or username incorrect')
+          : of(user)
+      )
+    );
+  }
 }

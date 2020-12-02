286ecfe314b0363270bd24acc7a53fbd59e79543
Simon Hessel <simon.hessel@kreios.lu>
2020-12-02 17:22:49 +0100

    backend: added serlizer middleware

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -5,6 +5,7 @@ import {
   ManyToMany,
   PrimaryGeneratedColumn,
 } from "typeorm";
+import { Exclude, Expose } from "class-transformer";
 import { Project } from "entities/project.entity";
 
 @Entity()
@@ -15,13 +16,15 @@ export class User {
   @Column()
   public email!: string;
 
+  @Exclude()
   @Column()
   public password!: string;
 
   @Column()
   public username!: string;
 
-  @ManyToMany(() => Project)
+  @Expose({ groups: ["user"] })
+  @ManyToMany(() => Project, (project) => project.users)
   @JoinTable()
   public projects!: Project[];
 }

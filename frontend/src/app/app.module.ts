6961eda26f2409a50ca920d949a7c7acdaad2c3c
Simon Hessel <simon.hessel@kreios.lu>
2020-12-18 10:59:21 +0100

    frontend: added baseurl interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { environment } from 'src/environments/environment';
+import { ApiBaseUrlInterceptors } from './services/ApiBaseUrlInterceptors';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
@@ -27,9 +29,20 @@ import { MatToolbarModule} from '@angular/material/toolbar';
     MatButtonModule,
     MatCardModule,
     MatCheckboxModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'API_BASE_DOMAIN',
+      useValue: environment.apiBaseDomain,
+    },
+    { provide: 'HTTPS', useValue: environment.https },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiBaseUrlInterceptors,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

fbce29591177b6623e29dcc590350969de32c0c7
Simon Hessel <simon.hessel@kreios.lu>
2020-12-20 21:00:38 +0100

    backend: added ParameterDecorator InjectRepository

--- backend/src/core/decorators/injectRepository.decorator.ts
+++ backend/src/core/decorators/injectRepository.decorator.ts
@@ -1,3 +1,10 @@
-export const InjectRepository = (): ParameterDecorator => {
-  return (target, propertyKey, parameterIndex) => {};
+import { ENTITIES_MEDTADATA } from "../constants";
+
+export const InjectRepository = (entity: unknown): ParameterDecorator => {
+  return (target, propertyKey, parameterIndex) => {
+    const repositories: Map<number, unknown> =
+      Reflect.getMetadata(ENTITIES_MEDTADATA, target) || new Map();
+    repositories.set(parameterIndex, entity);
+    Reflect.defineMetadata(ENTITIES_MEDTADATA, repositories, target);
+  };
 };

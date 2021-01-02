9e54f6c0142b6d920c2994f6d1cf5a1794a78abf
Annika <annika.rebekka.bach@rwth-aachen.de>
2021-01-02 22:01:41 +0100

    frontend: register snackbar module included

--- frontend/src/app/modules/auth/components/register/register.component.ts
+++ frontend/src/app/modules/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -16,9 +18,17 @@ export class RegisterComponent {
     password: new FormControl('', [Validators.required]),
   });
 
+  constructor(private snackBar: MatSnackBar, private authService: AuthService ){}
   onSubmit = () => {
+    this.authService.register(
+      this.registerForm.controls.eMail.value,
+      this.registerForm.controls.username.value,
+      this.registerForm.controls.password.value
+    ).subscribe()
+    /*
     console.log(this.registerForm.controls.eMail.value);
     console.log(this.registerForm.controls.username.value);
     console.log(this.registerForm.controls.password.value);
+    */
   };
 }

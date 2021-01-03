b6c6db296e426078b7f0064bc5e495080ba76add
Annika <annika.rebekka.bach@rwth-aachen.de>
2021-01-03 15:25:06 +0100

    backend: email service changes

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -1,5 +1,5 @@
-import { InjectRepository, Injectable } from 'core';
-import nodemailer  from "nodemailer";
+import { InjectRepository, Injectable } from "core";
+import nodemailer from "nodemailer";
 import { Repository } from "typeorm";
 import { User } from "entities/user.entity";
 import { UserService } from "services/user.service";
@@ -8,77 +8,71 @@ import { UserService } from "services/user.service";
 export class EmailService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
-    private readonly userService: UserService   
-  ) {}
-  
+    private readonly userService: UserService
+  ) { }
+
   private transport = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-    user: 'webtechtestnodemailer@gmail.com',
-    pass: 'webtech2020'
-    }
-  })
-  
+      user: "webtechtestnodemailer@gmail.com",
+      pass: "webtech2020",
+    },
+  });
+  // welcome new user and ask to verify email
   public async sendRegisterMail(username: string) {
-    
     const user = await this.userService.findUserByName(username);
-    
+
     const mailOptions = {
       from: '"Issue Tracker " <webtechtestnodemailer@gmail.com>',
       to: user?.email,
-      subject: 'Welcome to the Issue Tracker',
-      html: '<h1> this is the register email </h1>'
-    }
-    
-    this.transport.sendMail(mailOptions, function (err, data){
-      if(err) {
-        console.log('error occured ', err)
-      } else {
-        console.log(`Email send to ${username} with ${data}`)
-      }
+      subject: "Welcome to the Issue Tracker",
+      html: "<h1>Hello"+username+"</h1></br> <p>Thank you for registering</p>",
+    };
 
-    })
+    this.transport.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        throw 'An error occured and email could not be sent'
+      } else {
+        console.log(`Email send to ${username} with ${data}`);
+      }
+    });
   }
 
   public async sendforgotPasswordMail(username: string) {
     const user = await this.userService.findUserByName(username);
-    
+
     const mailOptions = {
       from: '"Issue Tracker " <webtechtestnodemailer@gmail.com>',
       to: user?.email,
-      subject: 'Changing your Password for the Issue Tracker',
-      html: '<h1 color="blue"> this is the forgot password mail </h1>'
-    }
-    
-    this.transport.sendMail(mailOptions, function (err, data){
-      if(err) {
-        console.log('error occured ', err)
-      } else {
-        console.log(`Email send to ${username} with ${data}`)
-      }
+      subject: "Password Recovery",
+      html: '<h1> this is the forgot password mail </h1>',
+    };
 
-    })
+    this.transport.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        console.log("error occured ", err);
+      } else {
+        console.log(`Email send to ${username} with ${data}`);
+      }
+    });
   }
-
-  public async sendinvitedToAProjectMail(username: string, projectID: number){
+  
+  public async sendinvitedToAProjectMail(username: string, projectID: number) {
     const user = await this.userService.findUserByName(username);
-    console.log(projectID);
+    
     const mailOptions = {
       from: '"Issue Tracker " <webtechtestnodemailer@gmail.com>',
       to: user?.email,
-      subject: 'Changing your Password for the Issue Tracker',
-      html: '<h1> this is the forgot password mail </h1>'
-    }
-    
-    this.transport.sendMail(mailOptions, function (err, data){
-      if(err) {
-        console.log('error occured ', err)
+      subject: "Invited to a Project",
+      html: "<h1> Invited to a Project </h1>",
+    };
+
+    this.transport.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        console.log("error occured ", err);
       } else {
-        console.log(`Email send to ${username} with ${data}`)
+        console.log(`Email send to ${username} with ${data}`);
       }
-
-    })
-  } 
+    });
+  }
 }
-
-

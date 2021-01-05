0ed51709289b1762d732711c329b2d62b743e0c8
Annika <annika.rebekka.bach@rwth-aachen.de>
2021-01-05 20:53:34 +0100

    backend: added links

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -3,6 +3,7 @@ import nodemailer from "nodemailer";
 import { Repository } from "typeorm";
 import { User } from "entities/user.entity";
 import { UserService } from "services/user.service";
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class EmailService {
@@ -12,66 +13,71 @@ export class EmailService {
   ) { }
 
   private transport = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.mailtrap.io",
+    port: 2525,
     auth: {
-      user: "webtechtestnodemailer@gmail.com",
-      pass: "webtech2020",
-    },
+      user: "2e3828af2c1ed4",
+      pass: "5c9f73c77c2960"
+    }
   });
-  // welcome new user and ask to verify email
+
   public async sendRegisterMail(username: string) {
     const user = await this.userService.findUserByName(username);
-
+    const randomToken = uuidv4();
+    // random token in datenbank speichern
+    const link = 'http://localhost:4200/auth/confirm/' + randomToken;
     const mailOptions = {
-      from: '"Issue Tracker " <webtechtestnodemailer@gmail.com>',
+      from: '"Issue Tracker " <smtp.mailtrap.io>',
       to: user?.email,
       subject: "Welcome to the Issue Tracker",
-      html: "<h1>Hello"+username+"</h1></br> <p>Thank you for registering</p>",
+      html: '<p>Hello '+username+'</p></br><p> Thank you for registering. To complete the registration process and verify your Account, click on this link: </br><a href='+link+'>Click here to verify</a></p>'
     };
 
     this.transport.sendMail(mailOptions, function (err, data) {
       if (err) {
         throw 'An error occured and email could not be sent'
       } else {
-        console.log(`Email send to ${username} with ${data}`);
+        console.log('Email send !!!');
       }
     });
   }
 
   public async sendforgotPasswordMail(username: string) {
     const user = await this.userService.findUserByName(username);
-
+    const randomToken = uuidv4();
+    const link = 'http://localhost:4200/auth/password-reset/'+randomToken
+    // randomToken in datenbank speichern
     const mailOptions = {
-      from: '"Issue Tracker " <webtechtestnodemailer@gmail.com>',
+      from: '"Issue Tracker " <smtp.mailtrap.io>',
       to: user?.email,
       subject: "Password Recovery",
-      html: '<h1> this is the forgot password mail </h1>',
+      html: '<p> Hey '+username+'. </p></br> <p> To reset your email please Click on this link: </br><a href='+link+'></a>Click here to reset Password </p>',
     };
 
     this.transport.sendMail(mailOptions, function (err, data) {
       if (err) {
-        console.log("error occured ", err);
+        throw 'An error occured and email could not be sent'
       } else {
-        console.log(`Email send to ${username} with ${data}`);
+        console.log('Email send !!!');
       }
     });
   }
   
   public async sendinvitedToAProjectMail(username: string, projectID: number) {
     const user = await this.userService.findUserByName(username);
-    
+    const link = 'http://localhost:4200/projects/'+projectID;
     const mailOptions = {
-      from: '"Issue Tracker " <webtechtestnodemailer@gmail.com>',
+      from: '"Issue Tracker " <smtp.mailtrap.io>',
       to: user?.email,
-      subject: "Invited to a Project",
-      html: "<h1> Invited to a Project </h1>",
+      subject: "You have been invited to a Project",
+      html: '<p>Hey '+username+' you have been invited to a Project. Click on this link to get to it</br><a href='+link+'>Click here</a></p>',
     };
 
     this.transport.sendMail(mailOptions, function (err, data) {
       if (err) {
-        console.log("error occured ", err);
+        throw 'An error occured and email could not be sent'
       } else {
-        console.log(`Email send to ${username} with ${data}`);
+        console.log('Email send !!!');
       }
     });
   }

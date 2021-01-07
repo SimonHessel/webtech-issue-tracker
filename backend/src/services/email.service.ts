55599982c137af2a28b812d8f516e35c539d830c
Annika <annika.rebekka.bach@rwth-aachen.de>
2021-01-07 14:41:36 +0100

    Verification Token

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -30,10 +30,15 @@ export class EmailService {
       from: '"Issue Tracker " <smtp.mailtrap.io>',
       to: user?.email,
       subject: "Welcome to the Issue Tracker",
-      html: '<p>Hello '+username+'</p></br><p> Thank you for registering. To complete the registration process and verify your Account, click on this link: </br><a href='+link+'>Click here to verify</a></p>'
+      html: '<p>Hello '+username+'</p></br><p> Thank you for registering. '+
+            'To complete the registration process and verify your Account, click on this link:'+
+            '</br><a href='+link+'>Click here to verify</a></p>'
     };
 
-    this.transport.sendMail(mailOptions, function (err, data) {
+    this.transport.sendMail(
+      mailOptions, 
+      function (err, data) 
+      {
       if (err) {
         throw 'An error occured and email could not be sent'
       } else {
@@ -44,14 +49,15 @@ export class EmailService {
 
   public async sendforgotPasswordMail(username: string) {
     const user = await this.userService.findUserByName(username);
-    const randomToken = uuidv4();
-    const link = 'http://localhost:4200/auth/password-reset/'+randomToken
+    const VerificationToken = uuidv4();
+    const link = 'http://localhost:4200/auth/password-reset/'+VerificationToken;
     // randomToken in datenbank speichern
     const mailOptions = {
       from: '"Issue Tracker " <smtp.mailtrap.io>',
       to: user?.email,
       subject: "Password Recovery",
-      html: '<p> Hey '+username+'. </p></br> <p> To reset your email please Click on this link: </br><a href='+link+'></a>Click here to reset Password </p>',
+      html: '<p> Hey '+username+'. </p></br> <p> To reset your email please Click on this link:' + 
+            '</br><a href='+link+'></a>Click here to reset Password </p>',
     };
 
     this.transport.sendMail(mailOptions, function (err, data) {
@@ -70,7 +76,8 @@ export class EmailService {
       from: '"Issue Tracker " <smtp.mailtrap.io>',
       to: user?.email,
       subject: "You have been invited to a Project",
-      html: '<p>Hey '+username+' you have been invited to a Project. Click on this link to get to it</br><a href='+link+'>Click here</a></p>',
+      html: '<p>Hey '+username+' you have been invited to a Project. Click on this link to get to it'+
+            '</br><a href='+link+'>Click here</a></p>',
     };
 
     this.transport.sendMail(mailOptions, function (err, data) {

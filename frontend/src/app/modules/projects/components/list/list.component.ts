61cb149862ecda6d1328667a1642374538c8badb
Simon Hessel <simon.hessel@kreios.lu>
2021-01-17 03:30:40 +0100

    frontend: updated list issues component

--- frontend/src/app/modules/projects/components/list/list.component.ts
+++ frontend/src/app/modules/projects/components/list/list.component.ts
@@ -1,7 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Issue } from 'core/models/issue.model';
 import { Project } from 'core/models/project.model';
 import { ProjectsService } from 'modules/projects/services/projects.service';
+import { distinctUntilChanged } from 'rxjs/internal/operators/distinctUntilChanged';
+import { filter } from 'rxjs/operators';
 import { UnsubscribeOnDestroyAdapter } from 'shared/utils/UnsubscribeOnDestroyAdapter';
 
 @Component({
@@ -14,10 +22,12 @@ export class ListComponent
   extends UnsubscribeOnDestroyAdapter
   implements OnInit {
   project: Project | undefined = undefined;
+  currentIssue: Issue | undefined = undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private readonly projectService: ProjectsService
+    private readonly projectService: ProjectsService,
+    private readonly cdRef: ChangeDetectorRef
   ) {
     super();
   }
@@ -27,8 +37,19 @@ export class ListComponent
     if (!id) throw new Error('Project guard malfunction');
 
     this.subs.sink = this.projectService.setCurrentProject(id).subscribe();
-    this.subs.sink = this.projectService.current.subscribe(
-      (project) => (this.project = project)
-    );
+
+    this.subs.sink = this.projectService.current
+      .pipe(
+        filter((project) => !!project),
+        distinctUntilChanged()
+      )
+      .subscribe((project) => {
+        this.project = project;
+        this.cdRef.markForCheck();
+      });
+  }
+
+  toggleSelectedIssue(issue: Issue) {
+    this.currentIssue = issue;
   }
 }

e48cdfa673d2edcbcebc2dcd84d918458ea10b89
Simon Hessel <simon.hessel@kreios.lu>
2021-01-17 03:32:59 +0100

    frontend: updated uses if project component

--- frontend/src/app/modules/projects/components/users/users.component.ts
+++ frontend/src/app/modules/projects/components/users/users.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Project } from 'core/models/project.model';
+import { ProjectsService } from 'modules/projects/services/projects.service';
+import { filter, distinctUntilChanged } from 'rxjs/operators';
+import { UnsubscribeOnDestroyAdapter } from 'shared/utils/UnsubscribeOnDestroyAdapter';
 
 @Component({
   selector: 'app-users',
@@ -6,8 +16,32 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersComponent implements OnInit {
-  constructor() {}
+export class UsersComponent
+  extends UnsubscribeOnDestroyAdapter
+  implements OnInit {
+  project: Project | undefined = undefined;
+  constructor(
+    private route: ActivatedRoute,
+    private readonly projectService: ProjectsService,
+    private readonly cdRef: ChangeDetectorRef
+  ) {
+    super();
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (!id) throw new Error('Project guard malfunction');
+
+    this.subs.sink = this.projectService.setCurrentProject(id).subscribe();
+
+    this.subs.sink = this.projectService.current
+      .pipe(
+        filter((project) => !!project),
+        distinctUntilChanged()
+      )
+      .subscribe((project) => {
+        this.project = project;
+        this.cdRef.markForCheck();
+      });
+  }
 }

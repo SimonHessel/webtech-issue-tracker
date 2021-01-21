e8386db5aaf243d0fbf725d4c548c5f7c69ada03
Annika <annika.rebekka.bach@rwth-aachen.de>
2021-01-21 13:54:24 +0100

    frontend: reset-password Form

--- frontend/src/app/modules/auth/components/reset-password/reset-password.component.ts
+++ frontend/src/app/modules/auth/components/reset-password/reset-password.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'core/services/auth.service';
+import { UnsubscribeOnDestroyAdapter } from 'shared/utils/UnsubscribeOnDestroyAdapter';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +14,26 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResetPasswordComponent implements OnInit {
+  resetPasswordForm = new FormGroup({
+    newPassword: new FormControl('', [
+      Validators.required, 
+      Validators.minLength(6)
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required, 
+      Validators.minLength(6)
+    ]),
+    
+  })
 
-  constructor() { }
+  constructor(
+    private snackBar: MatSnackBar,
+    private authService: AuthService
+  ) { }
 
+  onSubmit = () => {
+    console.log("success");
+  }
   ngOnInit(): void {
   }
 
